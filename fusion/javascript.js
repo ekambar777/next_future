7fa376988701416f210aa2c32533b0c5866c658f
Ekambar <ekambarmohanta8249@gmail.com>
2023-05-14 10:06:46 +0530

    song name is updated

--- fusion/javascript.js
+++ fusion/javascript.js
@@ -8,30 +8,31 @@ let play1 = document.getElementById('play1')
 let name_of_song =Array.from(document.getElementsByClassName('name_of_song'))
 let songItem = Array.from(document.getElementsByClassName('songItem'))
 let w = document.documentElement.clientWidth || window.innerWidth;
+// let displayName = document.getElementById('displayName')
 
 let songs = [
-  {song_name:'song1', song_path:'songs/1.mp3'},
-  {song_name:'song2', song_path:'songs/2.mp3'},
-  {song_name:'song3', song_path:'songs/3.mp3'},
-  {song_name:'song4', song_path:'songs/4.mp3'},
-  {song_name:'song5', song_path:'songs/5.mp3'},
-  {song_name:'song6', song_path:'songs/6.mp3'},
-  {song_name:'song7', song_path:'songs/7.mp3'},
-  {song_name:'song8', song_path:'songs/8.mp3'},
-  {song_name:'song9', song_path:'songs/9.mp3'},
-  {song_name:'song10', song_path:'songs/10.mp3'},
-  {song_name:'song11', song_path:'songs/11.mp3'},
-  {song_name:'song12', song_path:'songs/12.mp3'},
-  {song_name:'song13', song_path:'songs/13.mp3'},
-  {song_name:'song14', song_path:'songs/14.mp3'},
-  {song_name:'song15', song_path:'songs/15.mp3'},
-  {song_name:'song16', song_path:'songs/16.mp3'},
-  {song_name:'song17', song_path:'songs/17.mp3'},
-  {song_name:'song18', song_path:'songs/18.mp3'},
-  {song_name:'song19', song_path:'songs/19.mp3'},
-  {song_name:'song20', song_path:'songs/20.mp3'},
-  {song_name:'song21', song_path:'songs/21.mp3'},
-  {song_name:'song22', song_path:'songs/22.mp3'},
+  {song_name:'song 1', song_path:'songs/1.mp3'},
+  {song_name:'song 2', song_path:'songs/2.mp3'},
+  {song_name:'song 3', song_path:'songs/3.mp3'},
+  {song_name:'song 4', song_path:'songs/4.mp3'},
+  {song_name:'song 5', song_path:'songs/5.mp3'},
+  {song_name:'song 6', song_path:'songs/6.mp3'},
+  {song_name:'song 7', song_path:'songs/7.mp3'},
+  {song_name:'song 8', song_path:'songs/8.mp3'},
+  {song_name:'song 9', song_path:'songs/9.mp3'},
+  {song_name:'song 10', song_path:'songs/10.mp3'},
+  {song_name:'song 11', song_path:'songs/11.mp3'},
+  {song_name:'song 12', song_path:'songs/12.mp3'},
+  {song_name:'song 13', song_path:'songs/13.mp3'},
+  {song_name:'song 14', song_path:'songs/14.mp3'},
+  {song_name:'song 15', song_path:'songs/15.mp3'},
+  {song_name:'song 16', song_path:'songs/16.mp3'},
+  {song_name:'song 17', song_path:'songs/17.mp3'},
+  {song_name:'song 18', song_path:'songs/18.mp3'},
+  {song_name:'song 19', song_path:'songs/19.mp3'},
+  {song_name:'song 20', song_path:'songs/20.mp3'},
+  {song_name:'song 21', song_path:'songs/21.mp3'},
+  {song_name:'song 22', song_path:'songs/22.mp3'},
 ]
 
 // play pause click
@@ -41,11 +42,13 @@ let songs = [
     if (w<=768){
       masterplay.classList.remove('fa-play')
       masterplay.classList.add('fa-pause')
+      displayName.innerHTML=songs[0].song_name;
     } else {
       masterplay.classList.remove('fa-play')
       masterplay.classList.add('fa-pause')
       play1.style.display = "block"
       pause1.style.display = "none"
+      displayName.innerHTML=songs[0].song_name;
     }
 
 } else if (audioElement.play || audioElement.currentTime >= 0){
@@ -60,7 +63,7 @@ let songs = [
     play1.style.display = "none"
   }
   }
-
+  
 })
 // events to listin
 audioElement.addEventListener('timeupdate', ()=>{
@@ -73,16 +76,18 @@ myProrgressBar.addEventListener('change', ()=>{
 
 })
 
-
 // Iteetion of song
 songItem.forEach((element,i)=> {
-  element.getElementsByClassName('name_of_song')[0].innerText = songs[i].song_name
+  element.getElementsByClassName('name_of_song')[0].innerText = songs[i].song_name;
+  // element.getElementById('displayName')[0].innerText = songs[i].song_name;
 });
 
 const sign=()=>{
 name_of_song.forEach((ele)=>{
   // masterplay.classList.add('fa-pause')
   // masterplay.classList.remove('fa-play')
+
+
 })
 }
 
@@ -95,6 +100,8 @@ name_of_song.forEach(element => {
     audioElement.play()
     masterplay.classList.add('fa-pause')
     masterplay.classList.remove('fa-play')
+    play1.style.display = "block"
+    pause1.style.display = "none"
   })
 });
 
@@ -108,6 +115,8 @@ document.getElementById('next').addEventListener('click', ()=>{
   audioElement.play()
   masterplay.classList.add('fa-pause')
   masterplay.classList.remove('fa-play')
+  play1.style.display = "block"
+  pause1.style.display = "none"
 })
 
 
@@ -121,5 +130,7 @@ document.getElementById('back').addEventListener('click', ()=>{
   audioElement.play()
   masterplay.classList.add('fa-pause')
   masterplay.classList.remove('fa-play')
+  play1.style.display = "block"
+  pause1.style.display = "none"
 })
 
